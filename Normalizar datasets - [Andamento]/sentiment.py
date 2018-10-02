a596a2e7a78872bc91e2c11ae01420d641f89987
mpgx5c <mateuspgx5@live.com>
2018-10-01 23:45:13 -0300

    sentiment

--- Normalizar datasets - [Andamento]/sentiment.py
+++ Normalizar datasets - [Andamento]/sentiment.py
@@ -1,12 +1,11 @@
 import tweepy
 from textblob import TextBlob
 
-# Step 1 - Authenticate
-consumer_key= 'XLRkRlb7G2seBeo3cvTJPhNCL'
-consumer_secret= 'sUQr6Jes8MOMRBXarlrSEclKUjdcvz1XwVuFSawujNfmkzaHcw'
 
-access_token='191826083-UghNS3YwFlUTmnGfyimRBwMmuVdRtRHMCUFfAI7w'
-access_token_secret='KyP9MlMpj0MbO3IS1UsdTpEPpBQGMlBGXyX74FqKeWhaL'
+consumer_key= ''
+consumer_secret= ''
+access_token=''
+access_token_secret=''
 
 
 auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
@@ -14,16 +13,13 @@ auth.set_access_token(access_token,access_token_secret)
 
 api = tweepy.API(auth)
 
-name = "jennybrucenyc"
+name = "Trump"
 tweetCount = 10
 results = api.user_timeline(id=name, count=tweetCount)
 
 for tweet in results:
    print (tweet.geo)
 
-'''
-tweets = api.search('Trump')
-'''
 for tweet in results:
     frase = TextBlob(tweet.text)
 
