36cf8621f79e03842cb264da8ffc6031dccf9141
mpgx5c <mateuspgx5@live.com>
2018-10-13 22:08:30 -0300

    remove Idonesia

--- Mining Idonésia/read.py
+++
@@ -1,9 +0,0 @@
-import json
-
-index = 0
-with open("data.json","r") as f:
-
-    for line in f:
-        tweet = json.loads(line)
-        print(index,'->>',tweet['text'].replace("\n",""))
-        index += 1
