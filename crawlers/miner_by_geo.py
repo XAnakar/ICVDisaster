ebaee68d20a8d8c3b462804688557b7b32d75cdd
Mateus Garcia <mateuspgx5@live.com>
2018-10-17 14:16:38 -0300

    Update Spaceblank | <Dependencies>

--- crawlers/miner_by_geo.py
+++ crawlers/miner_by_geo.py
@@ -2,7 +2,6 @@ from twitter import *
 import sys, csv, json, time
 from last_id import  get_lastId
 
- 
 latitude    =   00
 longitude   =   00    
 max_range   =   00 #Km²
@@ -12,10 +11,8 @@ access_token_secret = 'chave'
 consumer_key        = 'chave'
 consumer_secret     = 'chave'
 
-
 twitter = Twitter(auth = OAuth(access_token, access_token_secret,consumer_key, consumer_secret))
 Saida = open("DATA_SET.json","a")
-
 
 result_count = 0
 last_id =  get_Lest_id("DATA_SET")
@@ -23,7 +20,7 @@ last_id =  get_Lest_id("DATA_SET")
 while True:
     try:
         query = twitter.search.tweets(q = "", geocode = "%f,%f,%dkm" % (latitude, longitude, max_range), count = 100, max_id = last_id)
-        
+    
         for result in query["statuses"]:
             if result["geo"]:
                 Data    = result["created_at"]
@@ -34,13 +31,10 @@ while True:
                 Saida.write(str(json.dumps(result)) + "\n")
                 result_count += 1
             last_id = result["id"]
-
     except:
         
         print(" Minerados até o momento -> [\x1b[31m%d\x1b[0m] Tweets..." % result_count)
         print(" Timeout Requests API... ")
         print(" \x1b[31mEsperando...\x1b[0m 15 min ")
-        
         time.sleep(15 * 60)
-        
         continue
