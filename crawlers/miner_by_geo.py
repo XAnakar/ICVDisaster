181f9f27ca3acb25958d522b7ba65eaaf10c8de4
Mateus Garcia <mateuspgx5@live.com>
2018-10-17 13:47:09 -0300

    UPDATE | last_id
    last_id serve para manter o ultimo estado em que o crawler parou de minerar

--- crawlers/miner_by_geo.py
+++ crawlers/miner_by_geo.py
@@ -1,5 +1,6 @@
 from twitter import *
 import sys, csv, json, time
+from last_id import  get_lastId
 
  
 latitude    =   00
@@ -13,30 +14,33 @@ consumer_secret     = 'chave'
 
 
 twitter = Twitter(auth = OAuth(access_token, access_token_secret,consumer_key, consumer_secret))
-Saida = open("data.json","a")
+Saida = open("DATA_SET.json","a")
+
 
 result_count = 0
+last_id =  get_lastId("DATA_SET")
+
 while True:
     try:
         query = twitter.search.tweets(q = "", geocode = "%f,%f,%dkm" % (latitude, longitude, max_range), count = 100, max_id = last_id)
         
         for result in query["statuses"]:
-            
-            Data    = result['created_at']
-            Name    = result['user']['screen_name']
-            
-            print('Usuário: '+ Name + '\nData:' + Data + '\n')
-
-            Saida.write(str(json.dumps(result)) + "\n")
-            result_count += 1
+            if result["geo"]:
+                Data    = result["created_at"]
+                Name    = result["user"]["screen_name"]
+                
+                print("Usuário: " + Name + "\n" + Data +"\n")
+    
+                Saida.write(str(json.dumps(result)) + "\n")
+                result_count += 1
+            last_id = result["id"]
 
     except:
         
-        print("Minerados até o momento -> [\x1b[31m%d\x1b[0m] Tweets..." % result_count)
-        print("Timeout Requests API...")
-        print("\x1b[31mEsperando...\x1b[0m 15 min")
+        print(" Minerados até o momento -> [\x1b[31m%d\x1b[0m] Tweets..." % result_count)
+        print(" Timeout Requests API... ")
+        print(" \x1b[31mEsperando...\x1b[0m 15 min ")
         
         time.sleep(15 * 60)
         
         continue
-
