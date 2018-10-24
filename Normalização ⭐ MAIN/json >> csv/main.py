939637c23bc760e844211b7fa4c9671999816e74
mpgx5c <mateuspgx5@live.com>
2018-10-24 03:41:05 -0300

    change json >> csv | make a new csv dataset

--- Normalização ⭐ MAIN/json >> csv/main.py
+++ Normalização ⭐ MAIN/json >> csv/main.py
@@ -6,24 +6,20 @@ saida = csv.writer(open('formated.csv', 'w'))
 fieldnames = ['id_str', 'screen_name', 'created_at','latitude','longitude','text']
 saida.writerow(fieldnames)
 
-
-#Verifica se a String por parâmetro se repete no csv ['formated.csv'], caso sim return True
-def count_query(query):
-    with open('formated.csv') as csvfile:
+def hit(query):
+    with open('RESULT.csv') as csvfile:
         reader = csv.DictReader(csvfile)
         for line in reader:
             if line['text'] == query:
                 return True
     return False
 
-
-#Grava no arquivo csv ['formated.csv'] de acordo com o retorno da função  count_query
 def make_csv():
 
-    for line in open('data_new.json'):
+    for line in open('DATASET.json'):
         data = json.loads(line)
 
-        if count_query(data['text']):
+        if hit(data['text']):
             print("Ocorrência!")
         else:
             geo   = data['geo'].replace("[","").replace("]","").split(',') 
