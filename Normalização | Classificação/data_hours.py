fdfe3aaeecd5ff37fd7b4e2407d3334d7c879f59
mpgx5c <mateuspgx5@live.com>
2018-10-24 04:25:21 -0300

    changes

--- Normalização | Classificação/data_hours.py
+++ Normalização | Classificação/data_hours.py
@@ -1,6 +1,6 @@
 import csv
 
-data = [line['created_at'] for line in csv.DictReader(open("/dataset result/formated.csv", encoding="utf8"))]
+data = [line['created_at'] for line in csv.DictReader(open("RESULT.csv", encoding="utf8"))]
 data.sort()
 
 print("Primeiro Tweet: ", min(data) + "\n" +
