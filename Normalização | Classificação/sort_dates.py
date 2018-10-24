de9f7f5e02ff4c5ff24168f8d24d75693405b4bd
mpgx5c <mateuspgx5@live.com>
2018-10-24 04:10:37 -0300

    sorting date done!

--- Normalização | Classificação/sort_dates.py
+++ Normalização | Classificação/sort_dates.py
@@ -1,5 +1,27 @@
 import json
+import pandas as pd
 
-geos = [json.loads(line)['geos'] for line in open('Clusters.json')]
-geos[0].sort
-print(geos[0])
+tweets_data = []
+
+import csv
+with open('RESULT.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        tweets_data.append(
+            row['created_at']
+        )
+
+tweets = map(lambda tweet: tweet, tweets_data)
+
+new = []
+
+for t in tweets:
+    new.append(t[8:19].replace(":"," "))
+
+
+for line in range(20):
+    print(new[line])
+print("---------------")
+new.sort()
+for line in range(20):
+    print(new[line])
