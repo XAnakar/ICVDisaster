d0bf2486990556cf1f208a25fab7079d93a183b3
mpgx5c <mateuspgx5@live.com>
2018-11-01 21:41:20 -0300

    news

--- Normalization/geo_plot.py
+++ Normalization/geo_plot.py
@@ -1,8 +1,6 @@
 from geopy.geocoders import Nominatim  
 import gmplot, csv
 
-
-# Go through all tweets and add locations to 'coordinates' dictionary
 coordinates = {'latitude': [], 'longitude': []}  
 for index in csv.DictReader(open("formated.csv","r")):  
     try:
@@ -11,12 +9,6 @@ for index in csv.DictReader(open("formated.csv","r")):
     except:
         pass
 
-# Instantiate and center a GoogleMapPlotter object to show our map
 gmap = gmplot.GoogleMapPlotter(30, 0, 3)
-
-
-# Insert points on the map passing a list of latitudes and longitudes
 gmap.heatmap(coordinates['latitude'], coordinates['longitude'], radius=50)
-
-# Save the map to html file
 gmap.draw("geoplot.html")  
